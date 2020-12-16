ef2e40bb6c10ebbde2b025be21c74ed987eb1e9e
Dan Soppelsa <dsoppelsa@gmail.com>
2020-12-15 23:33:51 -0500

    Test

---
+++ carbon-macros-helper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Illuminate\Support {
+    /**
+     * Carbon
+     * @method bool isNewYearsDay
+     * @method bool isEasterSunday
+     * @method bool isGoodFriday
+     * @method bool isFamilyDay
+     * @method bool isVictoriaDay
+     * @method bool isCanadaDay
+     * @method bool isLabourDay
+     * @method bool isCanadianThanksgiving
+     * @method bool isRemembranceDay
+     * @method bool isChristmasDay
+     * @method bool isBoxingDay
+     * @method bool isCivicHoliday
+     * @method bool isMlkJrDay
+     * @method bool isIndependenceDay
+     * @method bool isMemorialDay
+     * @method bool isLaborDay
+     * @method bool isVeteransDay
+     * @method bool isAmericanThanksgiving
+     * @method bool isPresidentsDay
+     * @method bool isColumbusDay
+     * @method bool isNewYearsEve
+     */
+    class Carbon extends \Carbon\Carbon {}
+}
